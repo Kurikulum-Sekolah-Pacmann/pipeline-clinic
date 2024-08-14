3aa9efee4a0488345a898088d6c2591fe96c0517
ihda <rosy2pacmannai.com>
2024-08-14 11:02:19 +0700

    create notebook live class

--- _pipeline.py
+++ _pipeline.py
@@ -1,5 +1,5 @@
-from warehouse_pipeline import pipeline_warehouse
-from staging_pipeline import pipeline_staging
+from _warehouse_pipeline import pipeline_warehouse
+from _staging_pipeline import pipeline_staging
 
 if __name__ == "__main__":
     pipeline_staging()
